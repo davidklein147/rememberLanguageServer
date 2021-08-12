0028e9de5135091feaa40c3eb4b0a9b87dbadcf1
davidklein147 <davidklein147@gmail.com>
2021-08-13 02:36:20 +0300

    app working

--- controllers/repetitionContro.js
+++ controllers/repetitionContro.js
@@ -1,6 +1,7 @@
 const con = require("../utils/sql");
 const revers = require("../utils/rtl_lang_reverse")
 const utilities = require("../utils/utilities")();
+const querys = require("../utils/querys")
 
 function repetitionContro() {
     function dailyRepetitionList(req, res) {
@@ -16,15 +17,32 @@ function repetitionContro() {
         where 
          a.UserId = ${req.params.id} and
          b.SourceId = a.Id and c.TranslateWordId = b.Id and
-         c.RepetitionDate <= ${utilities.setRepetitionDate(new Date(new Date().setDate(new Date().getDate() +1)))}`,
+         c.RepetitionDate <= ${utilities.setRepetitionDate(
+             //new Date(new Date().setDate(new Date().getDate() +1))
+             new Date()
+             )}`,
         (err, results) =>{
            err? utilities(res, err): 
            res.status(200).send(results)
         })
 
     }
+
+    function addNextDate(req, res){
+        if(!req.body){
+            res.status(400).send({mas :"missing details"})
+        }
+        con.query(querys.objToInsertQuery("REPETITION_DATA", req.body),
+        (err, newDate) =>{
+            if(err){
+                utilities.errors(res, err)
+            }
+            res.status(201).send(newDate);
+        })
+    }
     return {
-        dailyRepetitionList
+        dailyRepetitionList,
+        addNextDate
     }
 }
 
--- routers/repetiRouter.js
+++ routers/repetiRouter.js
@@ -4,6 +4,7 @@ const repetitionContro = require("../controllers/repetitionContro")
 
 const router = express.Router();
 router.get("/dailylist/:id", repetitionContro.dailyRepetitionList);
+router.post("/nextdate", repetitionContro.addNextDate);
 
 
 module.exports = router;
